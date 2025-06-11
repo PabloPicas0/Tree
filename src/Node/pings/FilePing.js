e67a13c1b60c7732f0e3e4dc6a32dda3e99d8dca
PabloPicas0 <pawel.fe@gmail.com>
2025-06-11 14:13:01 +0200

    feat: add exception when trying to rewrite existing file

--- src/Node/pings/FilePing.js
+++ src/Node/pings/FilePing.js
@@ -2,8 +2,14 @@ import fs from "node:fs/promises";
 
 // const filters = [{ name: "tree", extensions: ["json"] }];
 
+// TODO: add error when operation failed
 async function filePing(e, name) {
-  return await fs.writeFile(`./trees/${name}.json`, "{}");
+  const activeTrees = await fs.readdir("./trees");
+  const fileNameExists = activeTrees.some((fileName) => fileName === `${name}.json`);
+
+  if (fileNameExists) return;
+
+  return await fs.writeFile(`./trees/${name}.json`, "{}", {});
 }
 
 export default filePing;
