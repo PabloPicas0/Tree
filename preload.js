dfdcb2f649b4959ab0718b146ccd505d74681306
PabloPicas0 <pawel.fe@gmail.com>
2025-06-14 14:59:35 +0200

    feat: add delete file ping

--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("dialog", {
   initTrees: () => ipcRenderer.invoke("initTrees"),
-  loadFile: (name) => ipcRenderer.invoke("file", name),
+  loadFile: (name) => ipcRenderer.invoke("loadFile", name),
+  deleteFile: (name) => ipcRenderer.invoke("deleteFile", name),
 });
