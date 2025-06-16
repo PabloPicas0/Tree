03b82c056a2bfc6ccd3f0e60322f4915f5531859
PabloPicas0 <pawel.fe@gmail.com>
2025-06-16 15:58:51 +0200

    feat: add dialog ping handler

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 import { app, ipcMain } from "electron";
 
-import loadFilePing from "./src/Node/pings/loadFilePing.js";
+import createFilePing from "./src/Node/pings/createFilePing.js";
 import initTrees from "./src/Node/pings/initTreesPing.js";
 import createWindow from "./src/Window/createWindow.js";
 import deleteFilePing from "./src/Node/pings/deleteFilePing.js";
+import openFilePathDialogPing from "./src/Node/pings/openFilePathDialogPing.js";
 
 function handlePings() {
-  ipcMain.handle("loadFile", loadFilePing);
-  ipcMain.handle("deleteFile", deleteFilePing);
   ipcMain.handle("initTrees", initTrees);
+  ipcMain.handle("createFile", createFilePing);
+  ipcMain.handle("deleteFile", deleteFilePing);
+  // ipcMain.handle("updateFile", updateFilePing)
+  ipcMain.handle("openFilePathDialog", openFilePathDialogPing);
 }
 
 app.whenReady().then(() => {
