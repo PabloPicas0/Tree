da4d55f47aa2024c51f47b812d33ed5ad2b2395f
PabloPicas0 <pawel.fe@gmail.com>
2025-06-16 16:01:08 +0200

    feat: add listener for picture file path

--- src/Window/OptionsFrontend.js
+++ src/Window/OptionsFrontend.js
@@ -8,11 +8,22 @@ class Trees extends TreesEngine {
     this.form = document.querySelector(".file-picker");
     this.exitModal = document.querySelector(".exit-modal");
     this.treeNameInput = document.querySelector("#tree-name");
+    this.treePhotoInput = document.querySelector("#photo");
 
     this.newTree.addEventListener("click", this.toggleModal.bind(this));
     this.exitModal.addEventListener("click", this.toggleModal.bind(this));
-    this.form.addEventListener("submit", this.addNewTree.bind(this));
     this.deleteTreeOption.addEventListener("click", this.deleteTree.bind(this));
+    this.treePhotoInput.addEventListener("click", this.getPhotoPath.bind(this));
+    this.form.addEventListener("submit", this.addNewTree.bind(this));
+  }
+
+  async getPhotoPath() {
+    const path = await dialog.openFilePathDialog();
+
+    if (!path) return
+
+    // this.treePhotoInput.value = this.treePhotoInput.textContent + " " + path;
+    this.treePhotoInput.dataset.path = path
   }
 
   async addNewTree(e) {
@@ -20,13 +31,14 @@ class Trees extends TreesEngine {
 
     const data = new FormData(this.form);
     const name = data.get("name").trim();
+    const photoPath = this.treePhotoInput.dataset.path
 
     if (name === "") {
       this.toggleModal();
       return;
     }
 
-    await dialog.loadFile(name);
+    await fileHandler.createFile(name, photoPath);
 
     this.toggleModal();
     super.reload();
@@ -35,13 +47,14 @@ class Trees extends TreesEngine {
   async deleteTree() {
     const name = this.deleteTreeOption.dataset.name;
 
-    await dialog.deleteFile(name);
+    await fileHandler.deleteFile(name);
     super.reload();
   }
 
   toggleModal() {
     this.modal.classList.toggle("modal-off");
     this.treeNameInput.value = "";
+    this.treePhotoInput.dataset.path = ""
   }
 }
 
