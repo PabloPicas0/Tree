b219434a4092b2baae1e11f3d2e9fd2972af368f
PabloPicas0 <pawel.fe@gmail.com>
2025-06-18 13:15:52 +0200

    feat: add file path to delete image with tree

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("fileHandler", {
   initTrees: () => ipcRenderer.invoke("initTrees"),
   createFile: (name, path) => ipcRenderer.invoke("createFile", name, path),
-  deleteFile: (name) => ipcRenderer.invoke("deleteFile", name),
+  deleteFile: (name, path) => ipcRenderer.invoke("deleteFile", name, path),
   updateFile: (key, data) => ipcRenderer.invoke("updateFile", key, data),
 });
 
