e50cfcddf6eac3d8199a1f3d8888d11947b1941b
PabloPicas0 <pawel.fe@gmail.com>
2025-06-24 16:04:22 +0200

    feat: add tree drawing functionality and better dummy data

--- src/Window/TreesEngine.js
+++ src/Window/TreesEngine.js
@@ -1,27 +1,133 @@
-const { select, scaleLinear, axisBottom, axisLeft, tree } = d3;
+// Links:
+// https://observablehq.com/@d3/tree/2#data
 
+const { select, scaleLinear, axisBottom, axisLeft, tree, hierarchy, ascending, curveStep, link } = d3;
+
+// NOTE: start with oldest known person 
+// TODO: resize don't work on tree
+// TODO: trees need to be adjusted to svg
 class TreesEngine {
   constructor() {
-    this.tree = select(".tree");
-    this.svg = this.tree.append("svg").attr("id", "tree").attr("width", "100%").attr("height", "100%");
+    this.treeElem = select(".tree");
+    this.svg = this.treeElem.append("svg").attr("id", "tree").attr("width", "100%").attr("height", "100%");
 
     this.treeState = {
       ...this.getParentSize(),
       padding: 16,
       data: {
-        name: "test",
+        name: "Me",
         children: [
-          { name: "test2", children: [] },
-          { name: "test3", children: [] },
-          { name: "test4", children: [] },
-          { name: "test5", children: [] },
+          {
+            name: "Mom",
+            children: [
+              {
+                name: "grandmother",
+                children: [
+                  { name: "grandpa2x", children: null },
+                  { name: "grandmother2x", children: null },
+                ],
+              },
+              {
+                name: "grandpa",
+                children: [
+                  { name: "grandpa2x", children: null },
+                  { name: "grandmother2x", children: null },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Dad",
+            children: [
+              {
+                name: "grandmother",
+                children: [
+                  { name: "grandpa2x", children: null },
+                  { name: "grandmother2x", children: null },
+                ],
+              },
+              {
+                name: "grandpa2x",
+                children: [
+                  { name: "grandpa2x", children: null },
+                  { name: "grandmother2x", children: null },
+                ],
+              },
+            ],
+          },
         ],
       },
     };
 
+    this.root = hierarchy(this.treeState.data);
+
+    this.dx = 35;
+    this.dy = this.treeState.width / (this.root.height + 1);
+    this.x0 = Infinity;
+    this.x1 = -Infinity;
+
+    this.tree = tree().nodeSize([this.dx, this.dy]);
+
+    this.root.sort((a, b) => ascending(a.data.name, b.data.name));
+
+    this.tree(this.root);
+
+    this.root.each((descendant) => {
+      if (descendant.x > this.x1) this.x1 = descendant.x;
+      if (descendant.x < this.x0) this.x0 = descendant.x;
+    });
+
+    this.height = this.x1 - this.x0 + this.dx * 2;
+
     this.createGrid();
+    this.createTree();
 
     window.addEventListener("resize", this.resizeGrid.bind(this));
+  }
+
+  createTree() {
+    // this.svg.attr("viewBox", [-this.dy / 3, this.x0 - this.dx, this.treeState.width, this.height]);
+    const [xMid, yMid] = [this.treeState.width / 2, this.treeState.height / 2];
+    const container = this.svg.append("g").attr("transform", `translate(${83}, ${yMid})`);
+
+    container
+      .append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#fafafa")
+      .attr("stroke-opacity", 0.7)
+      .attr("stroke-width", 1.5)
+      .selectAll()
+      .data(this.root.links())
+      .join("path")
+      .attr(
+        "d",
+        link(curveStep)
+          .x((d) => d.y)
+          .y((d) => d.x)
+      );
+
+    const node = container
+      .append("g")
+      .attr("stroke-linejoin", "miter")
+      .attr("stroke-width", 3)
+      .selectAll()
+      .data(this.root.descendants())
+      .join("g")
+      .attr("transform", (d) => `translate(${d.y},${d.x})`);
+
+    node
+      .append("circle")
+      .attr("fill", "#fff")
+      .attr("r", 2.5);
+
+    node
+      .append("text")
+      .attr("dy", "0.31em")
+      .attr("x", (d) => (d.children ? -6 : 6))
+      .attr("text-anchor", (d) => (d.children ? "end" : "start"))
+      .text((d) => d.data.name)
+      .attr("stroke", "white")
+      .attr("paint-order", "stroke");
   }
 
   resizeGrid() {
@@ -41,7 +147,7 @@ class TreesEngine {
   }
 
   getParentSize() {
-    const { width, height } = this.tree.node().getBoundingClientRect();
+    const { width, height } = this.treeElem.node().getBoundingClientRect();
     const padding = 8 * 2;
 
     return { width: width - padding, height: height - padding };
