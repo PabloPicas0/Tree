04682121064919775961c0ce767be741f65b40da
PabloPicas0 <pawel.fe@gmail.com>
2025-08-01 22:03:53 +0200

    feat: add tree loading based on events

--- renderer.js
+++ renderer.js
@@ -1,5 +1,14 @@
+import CustomEvents from "./src/Window/CustomEvents.js";
 import Options from "./src/Window/OptionsFrontend.js";
 import TreesEngine from "./src/Window/TreesEngine.js";
 
-const options = new Options();
-const trees = new TreesEngine();
+const events = new CustomEvents();
+events.setEvent("reload");
+
+const options = new Options(events.listener, events.getEvenet("reload"));
+const trees = new TreesEngine(options.state.data[0]);
+const reloadTree = () => trees.reload(options.state.data[0]);
+
+events.listener.addEventListener("reload", reloadTree);
+window.addEventListener("resize", reloadTree);
+
