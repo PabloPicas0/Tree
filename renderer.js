c1ffba422b7ea4af50eb072b51867eb4d37237d7
PabloPicas0 <pawel.fe@gmail.com>
2025-08-06 14:20:20 +0200

    feat: add handleChange function

--- renderer.js
+++ renderer.js
@@ -5,10 +5,16 @@ import TreesEngine from "./src/Window/TreesEngine.js";
 const events = new CustomEvents();
 events.setEvent("reload");
 
-const options = new Options(events.listener, events.getEvenet("reload"));
+const options = new Options(events);
 const trees = new TreesEngine(options.state.data[0]);
 const reloadTree = () => trees.reload(options.state.data[0]);
+const handleChange = (e) => {
+  const { valueAsNumber, id } = e.target;
+  trees.state[id] = valueAsNumber;
+  trees.update();
+};
 
 events.listener.addEventListener("reload", reloadTree);
-window.addEventListener("resize", reloadTree);
-
+options.treeWidthSilider.addEventListener("input", handleChange);
+options.treeHeightSilider.addEventListener("input", handleChange);
+// window.addEventListener("resize", reloadTree);
